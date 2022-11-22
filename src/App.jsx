3a8a5647ef5ed09ef220c4553054adf27a5a6ce4
Miguel Silva <miguelsilvavianaa@gmail.com>
2022-11-21 22:13:26 -0300

    template
    template cru

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+export default props => {
+    return (
+        <div>
+            <h1>
+                Olá mundo mundo
+            </h1>
+        </div>
+    )
+}
